256388d32ac67857cee1af24e5b38c7ddcad34ce
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-07-18 21:55:20 +0800

    Create config-user.sh

---
+++ config-user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "输入增加用户的数量:"
+read -P "(请输入大于0的整数):" tempnum
+num=${tempnum}
+for ((i=1;i<=${num};i++ ));
+do
+       uname=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
+       echo "${uname} suansuanru.site" >> psw-file
+done

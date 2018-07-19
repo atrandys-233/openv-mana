26b7a358324b331ef98316965afacb87f4a6577f
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-07-19 09:46:32 +0800

    Update config-user.sh

--- config-user.sh
+++ config-user.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+ippool=（1 5 9 13 17 21 25 29 33 37 41 45 49 53 57 61 65 69 73 77 81 85 89 93 97 101）
 echo "输入增加用户的数量:"
 read -p "(请输入大于0的整数):" tempnum
 num=${tempnum}
 for ((i=1;i<=${num};i++ ));
 do
        uname=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
-       echo "${uname} suansuanru.site" >> psw-file
+       echo "${uname} suansuanru.site" > psw-file
+       echo "ifconfig-push 10.8.0.ippool[i] 10.8.0.{ipppol[i]+1}" > ${uname}
 done

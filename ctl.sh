547f34c135c6df31b4b3e6127afb008157f9b867
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-07-23 11:17:40 +0800

    Update ctl.sh

--- ctl.sh
+++ ctl.sh
@@ -1,4 +1,6 @@
 #查询当前流量并写入stat文件
 iptables -n -v -L -t filter -x| awk '$3=="ACCEPT" && $9 ~ /10.8.0./ {print $9,$2}' > /etc/openvpn/stat
 
-#stat数据与limit数据对比，进行处理
+#stat数据与limit数据合并
+awk  'FNR==NR{a[$1]=$1}NR>FNR{print $1,a[$2],$3,$5}' stat trafffic-limit >trafffic-now 
+

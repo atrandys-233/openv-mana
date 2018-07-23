986fd56a7a1617764699775d03f478f70b48197c
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-07-23 11:43:03 +0800

    Update ctl.sh

--- ctl.sh
+++ ctl.sh
@@ -2,5 +2,5 @@
 iptables -n -v -L -t filter -x| awk '$3=="ACCEPT" && $9 ~ /10.8.0./ {print $9,$2}' > /etc/openvpn/stat
 
 #stat数据与limit数据合并
-awk  'FNR==NR{a[$1]=$1}NR>FNR{print $1,a[$2],$3,$5}' stat trafffic-limit >trafffic-now 
+awk  'FNR==NR{a[$1]=$2}NR>FNR{if($2 in a)print $1,$2,$3,a[$2]}' stat trafffic-limit >trafffic-now
 

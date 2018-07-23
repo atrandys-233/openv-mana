043ee1193b8f7c366cba945b6d2c67dd428d5ac3
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-07-23 18:24:32 +0800

    Update ctl.sh

--- ctl.sh
+++ ctl.sh
@@ -4,3 +4,5 @@ iptables -n -v -L -t filter -x| awk '$3=="ACCEPT" && $9 ~ /10.8.0./ {print $9,$2
 #stat数据与limit数据合并
 awk  'FNR==NR{a[$1]=$2}NR>FNR{if($2 in a)print $1,$2,$3,a[$2]}' stat trafffic-limit >trafffic-now
 
+#处理数据
+awk '{if($4>$3){iptables -I FORWARD -d $2 -j DROP}}'
